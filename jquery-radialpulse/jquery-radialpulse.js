40e9fe57d964bd95c89f7354e5ee8eb1bf65cda7
Daniel Ivanovic <dan.ivanovic@antiblanks.com>
2015-03-06 14:35:35 +0000

    ADDED: Pure CSS solution for compatible browsers

--- jquery-radialpulse/jquery-radialpulse.js
+++ jquery-radialpulse/jquery-radialpulse.js
@@ -1,5 +1,5 @@
 if (!window["$"]) {
-	console.log("jquery-radialpulse.js: Error >> JQuery must be defined");
+	//console.log("jquery-radialpulse.js: Error >> JQuery must be defined");
 }
 
 /**
@@ -30,39 +30,75 @@ if (!window["$"]) {
 			"radialImage": "img/radial.png",
 			"animationSpeed": 1000,
 			"animationEasing": "linear",
-			"spawnSpeed": 150
+			"spawnSpeed": 150,
+			"borderColor": "#61959d",
+			"borderStartWidth": 2,
+			"borderEndWidth": 5
 	  	}, options);
 
+	  	// utils
+
+	  	function _isBorderRadiusSupported() {
+	  		var cssAttributeNames = ['borderRadius', 'BorderRadius', 'MozBorderRadius', 'WebkitBorderRadius', 'OBorderRadius', 'KhtmlBorderRadius']; 
+			for (var i = 0; i < cssAttributeNames.length; i++) {
+			    var attributeName = cssAttributeNames[i];
+			    if (window.document.body.style[attributeName] !== undefined) {
+			        return true;
+			    }
+			}
+			return false;
+	  	};
+
+	  	// private 
+
 	  	function spawnRadialPulse(target, delay) {
-	  		console.log("spawnRadialPulse", target, delay);
 	  		if ($(target).length == 0)
 	  			return false;
-	  		var radialImageTag = $("<img />")
-	  			.attr("src", options.radialImage)
-	  			.css("position", "absolute")
-	  			.css("left", $(target).position().left + parseInt($(target).css("margin-left")))
-	  			.css("top", $(target).position().top + parseInt($(target).css("margin-top")))
-	  			.css("width", $(target).width())
-	  			.css("height", $(target).height())
-	  			.css("opacity", options.startOpacity)
-	  			.css("filter", options.startBlur)
-				.css("webkitFilter", options.startBlur)
-				.css("mozFilter", options.startBlur)
-				.css("oFilter", options.startBlur)
-				.css("msFilter", options.startBlur);
-	  		$(target).before(radialImageTag);
-	  		$(radialImageTag).stop().delay(delay).velocity({
-	  			"width": $(target).width() * options.endScale,
-	  			"height": $(target).height() * options.endScale,
-	  			"left": $(target).position().left + parseInt($(target).css("margin-left")) + ($(target).width()/2) - (($(target).width() * options.endScale)/2),
-	  			"top": $(target).position().top + parseInt($(target).css("margin-top")) + ($(target).height()/2) - (($(target).height() * options.endScale)/2),
+	  		var radialTag;
+	  		var animationStartObj = {
+	  			"position": "absolute",
+	  			"left": ($(target).position().left + parseInt($(target).css("margin-left"))) + "px",
+	  			"top": ($(target).position().top + parseInt($(target).css("margin-top"))) + "px",
+	  			"width": $(target).width() + "px",
+	  			"height": $(target).height() + "px",
+	  			"opacity": options.startOpacity,
+	  			"filter": options.startBlur,
+				"webkitFilter": options.startBlur,
+				"mozFilter": options.startBlur,
+				"oFilter": options.startBlur,
+				"msFilter": options.startBlur
+	  		};
+	  		var animationEndObj = {
+	  			"width": ($(target).width() * options.endScale) + "px",
+	  			"height": ($(target).height() * options.endScale) + "px",
+	  			"left": ($(target).position().left + parseInt($(target).css("margin-left")) + ($(target).width()/2) - (($(target).width() * options.endScale)/2)) + "px",
+	  			"top": ($(target).position().top + parseInt($(target).css("margin-top")) + ($(target).height()/2) - (($(target).height() * options.endScale)/2)) + "px",
 	  			"opacity": options.endOpacity,
 	  			"filter": options.endBlur,
 	  			"webkitFilter": options.endBlur,
 	  			"mozFilter": options.endBlur,
 	  			"oFilter": options.endBlur,
 	  			"msFilter": options.endBlur
-	  		}, {
+	  		};
+	  		if (_isBorderRadiusSupported()) {
+	  			radialTag = $("<div />");
+	  			animationStartObj["webkitBorderRadius"] = "50%";
+	  			animationStartObj["mozBorderRadius"] = "50%";
+	  			animationStartObj["oBorderRadius"] = "50%";
+	  			animationStartObj["msBorderRadius"] = "50%";
+	  			animationStartObj["borderRadius"] = "50%";
+	  			animationStartObj["width"] = ($(target).width() - (options.borderStartWidth*2)) + "px";
+	  			animationStartObj["height"] = ($(target).height() - (options.borderStartWidth*2)) + "px";
+	  			animationStartObj["border"] = options.borderStartWidth + "px solid " + options.borderColor;
+	  			animationEndObj["border"] = options.borderEndWidth + "px solid " + options.borderColor;
+	  		}
+	  		else {
+	  			radialTag = $("<img />");
+	  			$(radialTag).attr("src", options.radialImage);
+	  		}
+	  		$(radialTag).css(animationStartObj);
+	  		$(target).before(radialTag);
+	  		$(radialTag).stop().delay(delay).velocity(animationEndObj, {
 	  			"easing": options.animationEasing,
 	  			"duration": options.animationSpeed,
 	  			"complete": function() {
@@ -72,10 +108,7 @@ if (!window["$"]) {
 	  		return true;
 	  	};
 
-	  	// private 
-
 	  	function pulse(target) {
-	  		console.log("pulse", target);
 	  		if (!$(target).hasClass("radial-pulse"))
 	  			return;
 	  		if (isNaN(options.numberOfRings))
